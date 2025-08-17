3e3bb11ed90b81e4a7276005b4e2d7c911c2c01e
Soumyajoy Pal <soumyajoypal@gmail.com>
2025-08-17 19:21:52 +0530

    fixed

--- client/src/lib/utils/apiRequest.js
+++ client/src/lib/utils/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://aquasavvy-2.onrender.com/api",
   withCredentials: true,
 });
 
